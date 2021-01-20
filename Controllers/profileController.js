296d01328e5b8e9d830ad2b4624bf2e00e562ad2
kojusola <adeola5678@gmail.com>
2021-01-20 10:11:01 +0100

    profile

--- Controllers/profileController.js
+++ Controllers/profileController.js
@@ -1,13 +1,14 @@
-const user = require("../models/user");
+const User = require("../models/user");
+const Profile = require("../models/profile");
 const upload = require("../utils/multer");
 const cloudinary = require("../utils/cloudinary");
 
 
 exports.getOneProfile= async(req, res) =>{
-    const userProfile = await user.findById({_id: req.params});
+    const userProfile = await Profile.findById({user_id: req.params.id});
     if(!userProfile){
         req.flash('error', 'Something went wrong. Try again')
-        return res.redirect('back')
+        return res.redirect('back');
     }
     res.status(200).json({
         status: "success",
@@ -18,7 +19,7 @@ exports.getOneProfile= async(req, res) =>{
 }
 
 exports.getAllProfile= async(req, res) =>{
-    const allUsersProfile = await user.find();
+    const allUsersProfile = await Profile.find();
     if(!allUsersProfile){
         req.flash('error', 'Something went wrong. Try again')
         return res.redirect('back')
@@ -32,7 +33,7 @@ exports.getAllProfile= async(req, res) =>{
 }
 
 exports.getAllFarmerProfile= async(req, res) =>{
-    const usersProfile = await user.find({role:'farmer',status: 'verified'});
+    const usersProfile = await Profile.find({role:'farmer',status: 'verified'});
     if(!usersProfile){
         req.flash('error', 'Something went wrong. Try again')
         return res.redirect('back')
@@ -44,26 +45,65 @@ exports.getAllFarmerProfile= async(req, res) =>{
         }
     })
 }
-// It can be used to update profile and also change users profile from pending to verified
+// // create profile
+
+exports.createProfile =  async(req, res)=>{
+    console.log(req.body)
+    const Profile = new Profile({
+        user_id: req.user._id,
+        name: req.user.name,
+        email: req.user.username,
+        role: req.user.role,
+        birthday: req.body.birthday,
+        address: req.body.address,
+        country: req.body.country,
+        phone: req.body.phone,
+        interests:req.body.interests     
+    })
+    console.log(profile)
+    await Profile.save();
+    req.flash('success', 'Profile updated successfully')
+    res.redirect(`/profile/${user_id}`);
+};
+
+// // It can be used to update profile and also change users profile from pending to verified
 exports.updateProfile =  async(req, res)=>{
-    const updatedProfile = await user.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true});
+    const updatedProfile = await Profile.findByIdAndUpdate({user_id: req.params.id}, req.body, {new: true});
     if(!updatedProfile){
         req.flash('error', 'Something went wrong. Try again')
         return res.redirect('back')
     }
     req.flash('success', 'Profile updated successfully')
-    res.redirect('/profile/:id')
+    res.redirect(`/profile/${user_id}`);
 };
 
 exports.updateProfileImage = async (req, res) => {
-    const result = await cloudinary.uploader.upload(req.file.path);
-        const updateProfile = await user.findByIdAndUpdate(req.params.id, {profileimage: {avatar:result.secure_url,cloundinary_id: result.public_id}}, {new: true})
+    const result = await cloudinary.uploader.upload(req.file.path); 
+        const updateProfile = await Profile.findByIdAndUpdate({user_id: req.params.id}, {profileimage: {avatar:result.secure_url,cloundinary_id: result.public_id}}, {new: true})
         if(!updateProfile){
             req.flash('error', 'Something went wrong. Try again')
             return res.redirect('back')
         }
         req.flash('success', 'Profile image updated successfully')
-        res.redirect('/profile/:id')  
+        res.redirect(`/profile/${user_id}`);
+        // return res.json({
+        //     updateProfile
+        // })
+}
+
+// posting images of the farm
+exports.updateFarmImage = async (req, res) => {
+    const files = req.files
+    for(const file of files){
+        const result = await cloudinary.uploader.upload(req.file.path);
+        const updateProfile = await Profile.findByIdAndUpdate({user_id: req.params.id}, {$push: {farmimage: {avatar:result.secure_url,cloundinary_id: result.public_id}}}, {new: true})
+        if(!updateProfile){
+            req.flash('error', 'Something went wrong. Try again')
+            return res.redirect('back')
+        }
+        req.flash('success', 'Profile image updated successfully')
+        res.redirect(`/profile/${user_id}`);
+    }
         // return res.json({
         //     updateProfile
         // })
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,14 +1,15 @@
 const router = require("express").Router()
 const upload = require("../utils/multer");
 const cloudinary = require("../utils/cloudinary");
-const { updateProfile, updateProfileImage,getAllFarmerProfile, getOneProfile,getAllProfile} = require("../controllers/profileController");
+const { updateProfile, updateProfileImage,getAllFarmerProfile,createProfile, getOneProfile,getAllProfile} = require("../controllers/profileController");
 const {isLoggedIn, restrictTo} = require("../middleware/authorization")
 
 
 router.route('/').get(getAllFarmerProfile);
 router.route('/all').get(getAllProfile);
-router.route('/:id').get(getOneProfile).patch(updateProfile);
-router.route('/:id/pimage').patch(upload.single('image'),updateProfileImage);
+router.route('/:id').get(getOneProfile).post(createProfile).patch(updateProfile);
+router.route('/:id/pimage').post(upload.single('image'),updateProfileImage);
+router.route('/:id/fimage').post(upload.array('image'),updateProfileImage);
 
 
 module.exports = router;
