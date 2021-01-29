4d135e68597c919db408b48a5b503ac36a6d9da9
charlene04 <charlesugbana04@gmail.com>
2021-01-29 10:50:45 +0100

    new

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
 	  secret: process.env.SESSION_SECRET,
 	  resave: false,
 	  saveUninitialized: false,
-	  cookie: { secure: false, maxAge: 60000 },
+	  cookie: { secure: false, maxAge: 3600000 },
 	  store: new MongoStore({
 		mongooseConnection: mongoose.connection,
 	  }),
