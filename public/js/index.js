61be460aa130c5631051c7bb17612485944c2400
charlene04 <charlesugbana04@gmail.com>
2021-01-29 09:38:44 +0100

    new

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,3 @@ let email = document.getElementById("profile").textContent
 const sub = email.split('@')
 document.getElementById("profile").textContent = sub[0]
 
-setTimeout(function(){
-    document.getElementsByClassName("alert")[0].style.display = 'none'
-}, 3000)
