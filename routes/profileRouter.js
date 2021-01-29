9059793fdd01863dfc6657da6ad8ccefd5a87145
charlene04 <charlesugbana04@gmail.com>
2021-01-29 10:23:12 +0100

    new

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -9,7 +9,7 @@ router.route('/').get(profileForm).post(createProfile);
 router.route('/all').get(getAllProfile);
 router.route('/:id').get(getOneProfile).patch(updateProfile);
 router.route('/:id/retrieve').get(retriveOneProfile)
-router.route('/:id/message').post(createMessage)
+router.route('/:id/message').post(isLoggedIn, createMessage)
 router.route('/:id/pimage').post(upload.single('image'),updateProfileImage);
 router.route('/:id/fimage').post(upload.array('image'),updateProfileImage);
 
