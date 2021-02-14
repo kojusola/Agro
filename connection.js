dd234b1a521e5a62e24e29367f72d0022f2d72b9
kojusola <adeola5678@gmail.com>
2021-02-14 20:01:22 +0100

    requested corrections

--- connection.js
+++ connection.js
@@ -9,3 +9,12 @@ exports.db = () => {
             console.log("ERROR", err)
         }
 })}
+
+// exports.db = () => {
+//     mongoose.connect("mongodb://localhost/agro", {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//         if(!err){
+//             console.log("Connection to database successful")
+//         }else{
+//             console.log("ERROR", err)
+//         }
+// })}
